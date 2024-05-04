8f0ae89963c8358683d13f15355daf442e04dc95
Void EUW <github.voideuw@gmail.com>
2024-05-04 03:41:29 +0200

    update

--- projects/projects.js
+++ projects/projects.js
@@ -1,75 +1,68 @@
+var project_list = {
+    "projects": [
+        {
+            "name": "project-odyssee",
+            "version": "(work in progess)",
+            "programming_languages": [
+                "HTML",
+                "CSS",
+                "Javascript"
+            ],
+            "description": "this project is secret..." 
+        },
+        {
+            "name": "project-miko",
+            "version": "(work in progress)",
+            "programming_languages": [
+                "Python"
+            ],
+            "description": "Project Miko is a discord bot thats moderating on servers, logging and so on (work in progress)"
+        },
+        {
+            "name": "graph-program",
+            "version": "1.0",
+            "programming_languages": [
+                "Python"
+            ],
+            "description": "A graph program that can show different graphs in your terminal"
+        }
+    ]
+};
+
 document.addEventListener("DOMContentLoaded", function() {
-    let projects_list = loadProjects();
-    //displayProjects(projects_list);
+    let projects = [];
+    project_list.projects.forEach(project => {
+        projects.push(project);
+    });
+    project_html = loadProjects(projects);
+    let projectList = document.getElementById('projectsList');
+    projectList.innerHTML = project_html
 });
 
-function loadProjects() {
-    fetch('projects.json') // Passe den Pfad entsprechend an
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Fehler beim Laden der Datei.');
-            }
-            return response.json();
-        })
-        .then(projects => {
-            const json = JSON.stringify(projects);
-            console.log(json);
-            // Hier kannst du weitere Logik ausführen oder die JSON-Daten anderweitig verwenden
-            return json;
-        })
-        .catch(error => {
-            console.error('Fehler:', error);
-        });
-}
+function loadProjects(projects) {
+    var loaded_projects = [];
+    projects.forEach(project => {
+        let name = project.name;
+        let version = project.version;
+        let programming_languages = "<p>Language: ";
+        let description = project.description;
 
-function displayProjects(name, programming_languages, description) {
+        project.programming_languages.forEach(language => {
+            programming_languages += `<strong>${language}</strong> `;
+        }); programming_languages += "</p>";
 
-}
+        loaded_projects += `
+            <div class="grid" style="grid-template-columns: 30% 68.64%;">
+                <article style="background: url('../assets/projects/${name}.png'); background-size: cover; background-position: center center;"></article>
+                <article>
+                    <h2><a href="">${name}</a></h2>
+                    <p>Version: ${version}</p>
+                    ${programming_languages}
+                    <p>${description}</p>
+                </article>
+            </div>
+        `
 
-function getSearchResults(content) {
-    let results = [];
-    search_list.players.forEach(player => {
-        if (player.name.toLowerCase().includes(content.toLowerCase()))
-            results.push(player);
     });
-    return results;
-}
-
-function displaySearchResults(results) {
-    output = "";
-    results.forEach(result => {
-        let name = result.name;
-        let profile_picture = result.profile_picture;
-        if(profile_picture.length < 1) {
-            profile_picture = "not_found.png"
-        }
-        let games_list = result.games, games_html = "";
-        let tags_list = result.tags, tags_html = `<p style="font-size: large; color: blueviolet">`;
-        let description = result.description;
-
-        games_list.forEach(game => {
-            games_html += `<img src="./assets/games/${game}.png" alt="${game}" style="width: 50px"></img>`;
-        });
-        tags_list.forEach(tag => {
-            tags_html += `${tag} `;
-        });
-        tags_html += "</p>";
-
-        output += `
-        <div class="grid" style="grid-template-columns: 30% 68.3%;">
-            <article style="background: url(./assets/users/${profile_picture}); background-size: cover; background-position: center center"></article>
-            <article>
-                <h3 style="margin-bottom: 5px">${name}<h3>
-                <div style="margin-bottom: 5px">
-                    ${games_html}
-                </div>
-                <div>
-                    ${tags_html}
-                </div>
-                <p style="font-size: large;">${description}</p>
-            </article>
-        </div>
-        `;
-    });
-    searchResults.innerHTML = output;
+    return loaded_projects;
 }
